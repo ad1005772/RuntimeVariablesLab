03989582b1397a6a968817725a68e91538bbb36c
ad1005772 <ad1005772@otc.edu>
2025-02-13 08:48:14 -0600

    Done

---
+++ RuntimeVariableTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RuntimeVariablesLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            string message = "Initial message";
+
+            for (int i = 0; i < 5; i++)
+            {
+                counter += i;
+                Console.WriteLine($"Loop {i}, Counter: {counter}");
+            }
+
+            Console.WriteLine($"Final Message: {message}");
+            Console.ReadLine();
+        }
+    }
+}
+
